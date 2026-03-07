3940ef888e02fe2085fbb58d07467b4c9a25aa6f
Filip Goldberger <filip.gold159@gmail.com>
2026-03-07 17:45:30 +0100

    Example code for testing

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import pygame
+import time
+import math
 
-def main():
-    print("Test")
+from world import World_2D, Car_2D
+from screen import Screen
+from graph import Graph
 
-if __name__ == "__main__":
-    main()
+from graph.colorCheckpoint import colorCheckpoint
+
+screen = Screen()
+clock = pygame.time.Clock()
+
+car = Car_2D(0.725, 3.4, -90, True)
+world = World_2D()
+graph = Graph(screen.size())
+
+# car.setSpeed(0.5)
+# car.setWheelAngle(math.radians(-15))
+car.init_draw(screen.size(), world.size())
+world.addEntity(car)
+
+try:
+    running = True
+    while running:
+        dt = clock.tick(24) / 1000.0
+        running = screen.update(world.getEntities())
+
+        colorCheckpoint(graph, car)
+        graph.draw(screen.getSurface())
+        world.update(dt)
+except KeyboardInterrupt:
+    print("\nUser pressed CTRL+C")
+    screen.quit()
+except Exception as e:
+    print(e)
